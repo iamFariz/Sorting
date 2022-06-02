e952d21eeee03011e0719348bb1b69764b3ac677
Fadila Rizka <92659712+iamFariz@users.noreply.github.com>
2022-06-02 18:33:59 +0700

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+public class BubbleSort {
+    private int[] data;
+
+    public BubbleSort(){
+
+    }
+    public int[] getData() {
+        return data;
+    }
+    public void setData(int[] data) {
+        this.data = data;
+    }
+    public void sortData(){
+        int lenData = this.data.length;
+        for(int i = 0;i<lenData;i++){
+            for (int j = i+1;j<lenData;j++){
+                if (this.data[j]<this.data[i]){
+                    int temp= this.data[i];
+                    this.data[i]= this.data[j];
+                    this.data[j]=temp;
+                }
+            }
+        }
+    }
+    public void PrintData(){
+        for (int datum : this.data){
+            System.out.println(datum +" ");
+        }
+    }
+    public static void main (String[] args){
+        int[] s=new int[]{10,35,32,13,26};
+        BubbleSort _myBubble = new BubbleSort();
+        _myBubble.setData(s);
+        _myBubble.sortData();
+        _myBubble.PrintData();
+    }
+}
